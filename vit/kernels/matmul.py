f88e7d9bc9cdcb8a69174243307e4c9f7d2164e6
romit <11757603+romitjain@users.noreply.github.com>
2024-04-23 19:48:50 +0530

    Addded batch support in matmul

--- vit/kernels/matmul.py
+++ vit/kernels/matmul.py
@@ -28,8 +28,10 @@ device = 'cuda:0'
 @triton.jit
 def matmul_kernel(
     A_ptr, B_ptr, O_ptr,
+    A_stride_batch,
     A_stride_height, A_stride_width,
     B_stride_height, B_stride_width,
+    O_stride_batch,
     O_stride_height, O_stride_width,
     M, N, K: tl.constexpr,
     bsx: tl.constexpr, bsy: tl.constexpr
@@ -41,10 +43,12 @@ def matmul_kernel(
     This can be further improved by implementing tiling, however
     I am yet to figure out how to use L2 cache in Triton.
     """
+    batch_idx = tl.program_id(axis=0)
     # Load apt data into memory
-    row_idx = tl.program_id(axis=0)
-    col_idx = tl.program_id(axis=1)
+    row_idx = tl.program_id(axis=1)
+    col_idx = tl.program_id(axis=2)
 
+    offset_batch = batch_idx * A_stride_batch
     offset_k = tl.arange(0, K)
 
     # Read offsets from A_ptr
@@ -52,40 +56,53 @@ def matmul_kernel(
     offset_a = offset_a[:, None]*A_stride_height + offset_k[None, :]*A_stride_width  # by * K
     mask_a = row_idx * bsy + tl.arange(0, bsy)
     mask_a = (mask_a[:, None] < M) & (offset_k[None, :] < K)
-    a = tl.load(A_ptr + offset_a)
+    a = tl.load(A_ptr + offset_batch + offset_a, mask_a)
 
     # Read offset from B_ptr
     offset_b = col_idx * bsx + tl.arange(0, bsx)
     offset_b = offset_k[:, None]*B_stride_height + offset_b[None, :]*B_stride_width  # K * bx
     mask_b = col_idx * bsx + tl.arange(0, bsx)
     mask_b = (offset_k[:, None] < K) & (mask_b[None, :] < N)
-    b = tl.load(B_ptr + offset_b)
+    b = tl.load(B_ptr + offset_b, mask_b)
 
     o = tl.dot(a, b)
 
+    offset_batch_out = batch_idx * O_stride_batch
     offset_or = row_idx * bsy + tl.arange(0, bsy)
     offset_oc = col_idx * bsx + tl.arange(0, bsx)
-    offset_o = offset_or[:, None]*O_stride_height\
-        + offset_oc[None, :]*O_stride_width  # by * bx
+    offset_o = offset_or[:, None]*O_stride_height+ offset_oc[None, :]*O_stride_width  # by * bx
     mask_o = (offset_or[:, None] < M) & (offset_oc[None, :] < N)
 
-    tl.store(O_ptr+offset_o, o, mask_o)
+    tl.store(O_ptr + offset_batch_out + offset_o, o, mask_o)
 
 def matmul_triton(A: torch.Tensor, B: torch.Tensor) -> torch.Tensor:
-    M, K = A.shape
+    """
+    Implements matrix multiplication between input matrix A and B
+    
+    Args:
+        - A {torch.Tensor}: Input matrix with shape (B, T, Cin) where B is the batch size, T is the sequence length, Cin is the input dimension
+        - B {torch.Tensor}: Weight matrix with shape (Cin, Cout) where Cout is the hidden dimension
+    """
+    assert len(A.shape) == 3, "First input matrix needs to have 3 dimensions (B, T, C)"
+    assert A.device == B.device and A.is_cuda, "Both matrix should be on GPU"
+
+    batch_size, M, K = A.shape
     K, N = B.shape
 
     # m, n = 16, 16
     # bsx, bsy = triton.next_power_of_2(n), triton.next_power_of_2(m)
-    grid = lambda meta: (triton.cdiv(M, meta["bsy"]), triton.cdiv(N, meta["bsx"]))
+    grid = lambda meta: (batch_size, triton.cdiv(M, meta["bsy"]), triton.cdiv(N, meta["bsx"]))
 
-    O = torch.empty((M, N)).to(device)
+    O = torch.empty((batch_size, M, N)).to(A.device, A.dtype)
+
     matmul_kernel[grid](
         A, B, O,
-        A.stride(0), A.stride(1),
-        B.stride(0), B.stride(1),
-        O.stride(0), O.stride(1),
-        M, N, K#, bsx=bsx, bsy=bsy
+        A_stride_batch=A.stride(0),
+        A_stride_height=A.stride(1), A_stride_width=A.stride(2),
+        B_stride_height=B.stride(0), B_stride_width=B.stride(1),
+        O_stride_batch=O.stride(0),
+        O_stride_height=O.stride(1), O_stride_width=O.stride(2),
+        M=M, N=N, K=K#, bsx=bsx, bsy=bsy
     )
 
     return O
@@ -97,27 +114,30 @@ if __name__ == '__main__':
     '''
     from argparse import ArgumentParser
     parser = ArgumentParser()
+    
+    parser.add_argument('-B', type=int)
     parser.add_argument('-M', type=int)
     parser.add_argument('-K', type=int)
     parser.add_argument('-N', type=int)
 
     args = parser.parse_args()
     print(f'Args: {args}')
+    batch_size = args.B
     M = args.M
     K = args.K
     N = args.N
 
-    A = torch.randint(0, 10, (M, K), device='cuda', dtype=torch.float32)
+    A = torch.randint(0, 10, (batch_size, M, K), device='cuda', dtype=torch.float32)
     B = torch.randint(0, 5, (K, N), device='cuda', dtype=torch.float32)
 
-    assert A.shape[1] == B.shape[0], 'Matrix are not compatible for multiplication'
+    assert A.shape[2] == B.shape[0], 'Matrix are not compatible for multiplication'
 
     y_pytorch = torch.matmul(A, B)
     y_triton = matmul_triton(A, B)
 
-    # print(f'Original matrix:\n{A}\n{B}')
-    # print(f'PyTorch:\n{y_pytorch}')
-    # print(f'Triton:\n{y_triton}')
+    print(f'Original matrix:\n{A}\n{B}')
+    print(f'PyTorch:\n{y_pytorch}')
+    print(f'Triton:\n{y_triton}')
 
     # Unit testing
     if torch.allclose(y_triton, y_pytorch, atol=1e-2, rtol=0):
--- vit/vit.py
+++ vit/vit.py
@@ -3,30 +3,69 @@ from torch import nn
 
 from kernels import patching, matmul
 
+device = 'cuda:0'
+
 class VIT(nn.Module):
     def __init__(
-        model_id: str,
+        self,
         height: int,
         width: int,
-        patch_size: int
+        channels: int,
+        patch_size: int,
+        hidden_dim: int
     ):
         super().__init__()
+
         assert height == width, "Height and width should be the same"
         assert height % patch_size == 0, "Height should be divisible by the patch size"
         assert width % patch_size == 0, "Width should be divisible by the patch size"
 
         self.height = height
-        self.widht = width
+        self.width = width
+        self.channels = channels
         self.patch_size = patch_size
+        self.hidden_dim = hidden_dim
         
+        num_patches = (self.height//self.patch_size) * (self.width//self.patch_size)
+        patch_dim = self.patch_size*self.patch_size*self.channels
+
         # Layers initialization
-        self.patching = patching
-        self.positional_embedding = None
+        self.projection = nn.Parameter(torch.randn(patch_dim, self.hidden_dim)).to(device, torch.float32)
+        self.positional_embedding = nn.Parameter(torch.randn(1, num_patches, self.hidden_dim)).to(device, torch.float32)
     
     def forward(self, x):
-        assert x.shape == (self.height, self.weight)
-        x = self.patching(x, self.patch_size, self.positional_embedding)
+        print(f'Image shape provided: {x.shape}')
+        assert x.shape[1:] == (3, self.height, self.width), f"Image size {x.shape[1:]} not matching with the model input size: {3, self.height, self.width}"
+
+        x = patching(x, self.patch_size) 
+        print(f'Shape after patching: {x.shape}')
+        
+        x = matmul(x, self.projection)
+        print(f'Shape after matmul: {x.shape}')
+        
         x += self.positional_embedding
+        print(f'Shape after positional embedding: {x.shape}')
 
         return x
 
+if __name__ == '__main__':
+    from PIL import Image
+    import requests
+    import numpy as np
+
+    model = VIT(
+                height=128,
+                width=128,
+                channels=3,
+                patch_size=4,
+                hidden_dim=256
+            )
+
+    url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+    image = Image.open(requests.get(url, stream=True).raw)
+    image = image.resize((128, 128))
+    image = torch.Tensor(np.array(image)).to(device='cuda', dtype=torch.float32)
+
+    print(f'Input image shape: {image.shape}')
+
+    out = model(image[None, :].permute(0, 3, 1, 2))
