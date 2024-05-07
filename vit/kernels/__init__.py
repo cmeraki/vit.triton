a4d4c23e2c5d5ce0d2f2205b3cb9f886e1547e82
romit <11757603+romitjain@users.noreply.github.com>
2024-05-07 14:56:19 +0530

    Updated layernorm file name

--- vit/kernels/__init__.py
+++ vit/kernels/__init__.py
@@ -2,4 +2,4 @@ from .patching import patching_triton as patching
 from .matmul import matmul_triton as matmul
 from .softmax import softmax_triton as softmax
 from .add import add_triton as add
-from .layer_norm import layernorm_triton as layernorm
+from .layernorm import layernorm_triton as layernorm
