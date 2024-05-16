cff78a9518c63d85412176d7f5d7bdc2706f0f0d
romit <11757603+romitjain@users.noreply.github.com>
2024-05-16 14:22:02 +0530

    Update matmul to use swizzle

--- vit/kernels/matmul.py
+++ vit/kernels/matmul.py
@@ -3,24 +3,26 @@ import triton
 import triton.language as tl
 
 device = 'cuda:0'
+dtype=torch.float16
 
 @triton.autotune(
   configs=[
-    triton.Config({'bsy': 256, 'bsx': 256, 'bsk': 256}, num_warps=16),
-    triton.Config({'bsy': 128, 'bsx': 128, 'bsk': 128}, num_warps=16),
-    triton.Config({'bsy': 64, 'bsx': 64, 'bsk': 64}, num_warps=16),
-    triton.Config({'bsy': 32, 'bsx': 32, 'bsk': 32}, num_warps=16),
-    triton.Config({'bsy': 16, 'bsx': 16, 'bsk': 16}, num_warps=16),
-    triton.Config({'bsy': 256, 'bsx': 256, 'bsk': 256}, num_warps=8),
-    triton.Config({'bsy': 128, 'bsx': 128, 'bsk': 128}, num_warps=8),
-    triton.Config({'bsy': 64, 'bsx': 64, 'bsk': 64}, num_warps=8),
-    triton.Config({'bsy': 32, 'bsx': 32, 'bsk': 32}, num_warps=8),
-    triton.Config({'bsy': 16, 'bsx': 16, 'bsk': 16}, num_warps=8),
-    triton.Config({'bsy': 256, 'bsx': 256, 'bsk': 256}, num_warps=4),
-    triton.Config({'bsy': 128, 'bsx': 128, 'bsk': 128}, num_warps=4),
-    triton.Config({'bsy': 64, 'bsx': 64, 'bsk': 64}, num_warps=4),
-    triton.Config({'bsy': 32, 'bsx': 32, 'bsk': 32}, num_warps=4),
-    triton.Config({'bsy': 16, 'bsx': 16, 'bsk': 16}, num_warps=4), 
+      triton.Config({'bsy': 128, 'bsx': 256, 'bsk': 64, 'group_sz': 8}, num_stages=3, num_warps=8),
+      triton.Config({'bsy': 64, 'bsx': 256, 'bsk': 32, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 128, 'bsx': 128, 'bsk': 32, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 128, 'bsx': 64, 'bsk': 32, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 64, 'bsx': 128, 'bsk': 32, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 128, 'bsx': 32, 'bsk': 32, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 64, 'bsx': 32, 'bsk': 32, 'group_sz': 8}, num_stages=5, num_warps=2),
+      triton.Config({'bsy': 32, 'bsx': 64, 'bsk': 32, 'group_sz': 8}, num_stages=5, num_warps=2),
+      triton.Config({'bsy': 128, 'bsx': 256, 'bsk': 128, 'group_sz': 8}, num_stages=3, num_warps=8),
+      triton.Config({'bsy': 256, 'bsx': 128, 'bsk': 128, 'group_sz': 8}, num_stages=3, num_warps=8),
+      triton.Config({'bsy': 256, 'bsx': 64, 'bsk': 128, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 64, 'bsx': 256, 'bsk': 128, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 128, 'bsx': 128, 'bsk': 128, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 128, 'bsx': 64, 'bsk': 64, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 64, 'bsx': 128, 'bsk': 64, 'group_sz': 8}, num_stages=4, num_warps=4),
+      triton.Config({'bsy': 128, 'bsx': 32, 'bsk': 64, 'group_sz': 8}, num_stages=4, num_warps=4)
   ],
   key=['M', 'N', 'K'],
 )
@@ -33,19 +35,23 @@ def matmul_kernel(
     O_stride_batch,
     O_stride_height, O_stride_width,
     M, N, K,
-    bsx: tl.constexpr, bsy: tl.constexpr, bsk: tl.constexpr
+    bsx: tl.constexpr, bsy: tl.constexpr, bsk: tl.constexpr, group_sz: tl.constexpr
 ):
     """
-    Matrix multiplication by loading rows of A
-    and columns of B to calculate a block of O.
+    Matrix multiplication by loading rows of A and columns of B to calculate a block of O.
+    Uses swizzle to improve L2 cache hit rate, inspired by
+    https://triton-lang.org/main/getting-started/tutorials/03-matrix-multiplication.html
 
-    This can be further improved by implementing tiling, however
-    I am yet to figure out how to use L2 cache in Triton.
+    Is there any performance gains to be done by using Swizzle for batch dimension?
     """
     batch_idx = tl.program_id(axis=0)
-    # Load apt data into memory
     row_idx = tl.program_id(axis=1)
     col_idx = tl.program_id(axis=2)
+
+    num_row_programs = tl.num_programs(1)
+    num_col_programs = tl.num_programs(2)
+
+    row_idxnew, col_idxnew = tl.swizzle2d(row_idx, col_idx, num_row_programs, num_col_programs, group_sz)
 
     offset_batch = batch_idx * A_stride_batch
 
@@ -55,16 +61,16 @@ def matmul_kernel(
         offset_k = offset + tl.arange(0, bsk)
 
         # Read offsets from A_ptr
-        offset_a = row_idx * bsy + tl.arange(0, bsy)
+        offset_a = row_idxnew * bsy + tl.arange(0, bsy)
         offset_a = offset_a[:, None]*A_stride_height + offset_k[None, :]*A_stride_width  # bsy * bsk
-        mask_a = row_idx * bsy + tl.arange(0, bsy)
+        mask_a = row_idxnew * bsy + tl.arange(0, bsy)
         mask_a = (mask_a[:, None] < M) & (offset_k[None, :] < K)
         a = tl.load(A_ptr + offset_batch + offset_a, mask_a)
 
         # Read offset from B_ptr
-        offset_b = col_idx * bsx + tl.arange(0, bsx)
+        offset_b = col_idxnew * bsx + tl.arange(0, bsx)
         offset_b = offset_k[:, None]*B_stride_height + offset_b[None, :]*B_stride_width  # bsk * bsx
-        mask_b = col_idx * bsx + tl.arange(0, bsx)
+        mask_b = col_idxnew * bsx + tl.arange(0, bsx)
         mask_b = (offset_k[:, None] < K) & (mask_b[None, :] < N)
         b = tl.load(B_ptr + offset_b, mask_b)
 
@@ -72,8 +78,8 @@ def matmul_kernel(
         output += o
 
     offset_batch_out = batch_idx * O_stride_batch
-    offset_or = row_idx * bsy + tl.arange(0, bsy)
-    offset_oc = col_idx * bsx + tl.arange(0, bsx)
+    offset_or = row_idxnew * bsy + tl.arange(0, bsy)
+    offset_oc = col_idxnew * bsx + tl.arange(0, bsx)
     offset_o = offset_or[:, None]*O_stride_height+ offset_oc[None, :]*O_stride_width  # bsy * bsx
     mask_o = (offset_or[:, None] < M) & (offset_oc[None, :] < N)
 
@@ -110,7 +116,7 @@ def matmul_triton(A: torch.Tensor, B: torch.Tensor) -> torch.Tensor:
         B_stride_height=B.stride(0), B_stride_width=B.stride(1),
         O_stride_batch=O.stride(0),
         O_stride_height=O.stride(1), O_stride_width=O.stride(2),
-        M=M, N=N, K=K#, bsx=bsx, bsy=bsy
+        M=M, N=N, K=K,
     )
 
     return O
@@ -135,8 +141,8 @@ if __name__ == '__main__':
     K = args.K
     N = args.N
 
-    A = torch.randint(0, 10, (batch_size, M, K), device='cuda', dtype=torch.float32)
-    B = torch.randint(0, 5, (K, N), device='cuda', dtype=torch.float32)
+    A = torch.randint(0, 10, (batch_size, M, K), device='cuda', dtype=dtype)
+    B = torch.randint(0, 5, (K, N), device='cuda', dtype=dtype)
 
     assert A.shape[2] == B.shape[0], 'Matrix are not compatible for multiplication'
 
@@ -153,7 +159,7 @@ if __name__ == '__main__':
     @triton.testing.perf_report(
         triton.testing.Benchmark(
             x_names=["M", "N", "K"],
-            x_vals=[64, 128, 256, 512, 1024, 2056, 4096],
+            x_vals=[128 * i for i in range(2, 33)],
             line_arg='provider',
             line_vals=[
                 'triton',
@@ -167,20 +173,20 @@ if __name__ == '__main__':
             ylabel="GB/s",
             plot_name="Performance",
             # values for function arguments not in `x_names` and `y_name`
-            args={'batch_size': 4},
+            args={'batch_size': 1},
         ))
     def benchmark(batch_size, M, N, K, provider):
         quantiles = [0.5, 0.2, 0.8]
 
-        A = torch.randint(0, 10, (batch_size, M, K), device='cuda', dtype=torch.float32)
-        B = torch.randint(0, 5, (K, N), device='cuda', dtype=torch.float32)
+        A = torch.randint(0, 10, (batch_size, M, K), device='cuda', dtype=dtype)
+        B = torch.randint(0, 5, (K, N), device='cuda', dtype=dtype)
 
         if provider == 'triton':
             ms, min_ms, max_ms = triton.testing.do_bench(lambda: matmul_triton(A, B), quantiles=quantiles)
         if provider == 'torch':
             ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch.matmul(A, B), quantiles=quantiles)
 
-        def gbps(ms): return 2 * M * N * K * 1e-12 / (ms * 1e-3)
+        def gbps(ms): return 2 * batch_size * M * N * K * 1e-12 / (ms * 1e-3)
 
         return gbps(ms), gbps(max_ms), gbps(min_ms)
 
